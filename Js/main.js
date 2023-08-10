0b2c45c552ec6c3fbacb9b2137bed00b18b2d94d
HusseinAli97 <venom.blackdesert@gmail.com>
2023-08-10 20:41:02 +0300

    first commit

---
+++ Js/main.js
@@ -0,0 +1,30 @@
+const joke = document.getElementById('joke');
+const jokeBtn = document.getElementById('getBtn');
+
+// const generateJoke = () => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+//     xhr.onreadystatechange = function () {
+//         if (this.readyState === 4 && this.status === 200) {
+//             let currantJoke = JSON.parse(this.responseText);
+//             joke.innerHTML = currantJoke.value;
+//         }
+//     }
+//     xhr.send();
+// }
+const xhr = new XMLHttpRequest();
+function getJoke() {
+    xhr.open('GET', 'https://api.chucknorris.io/jokes/random');    xhr.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+            let currantJoke = JSON.parse(this.responseText);
+            joke.innerHTML = currantJoke.value;
+            }else{
+            joke.innerHTML = 'Loading...';
+        }
+        }
+    }
+    xhr.send();
+}
+jokeBtn.addEventListener('click', getJoke);
+document.addEventListener('DOMContentLoaded', getJoke);
